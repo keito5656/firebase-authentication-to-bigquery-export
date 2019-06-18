a37a7353fd1ee5a518a9d68e225118d4cdd9904c
keito5656 <b08109@gmail.com>
2019-06-18 10:42:37 +0900

    add files

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-# firebase-authentication-to-bigquery-export
+# Firebase Authentication to BigQuery export
+
+An automatic tool for copying and converting Firebase Authentication data to BigQuery.
+Inspired by [firestore-to-bigquery-export](https://github.com/Johannes-Berggren/firestore-to-bigquery-export)
+
+
+- Create a BigQuery dataset with tables corresponding to your Firebase Authentication.
+- This package doesn't write anything to Firebase Authentication.
+
+## Installation
+> npm i firebase-authentication-to-bigquery-export
+
+```javascript
+import bigExport from 'firebase-authentication-to-bigquery-export'
+
+// or
+
+const bigExport = require('firebase-authentication-to-bigquery-export')
+
+// then
+
+const GCPSA = require('./Your-Service-Account-File.json')
+bigExport.setBigQueryConfig(GCPSA)
+bigExport.setFirebaseConfig(GCPSA)
+```
+
+## How to
+
+### API
+```javascript
+bigExport.setBigQueryConfig(
+  serviceAccountFile // JSON
+)
+```
+
+```javascript
+bigExport.setFirebaseConfig(
+  serviceAccountFile // JSON
+)
+```
+
+
+```javascript
+bigExport.createBigQueryTables()
+```
+
+```javascript
+bigExport.copyToBigQuery(
+    verbose // Boolean
+)
+```
+
+```javascript
+bigExport.deleteBigQueryTables()
+```
+
+
+### Examples
+
+```javascript
+/* 
+Initialize BigQuery dataset named 'authentication' with four firebase authentication.
+ */
+
+bigExport.createBigQueryTables()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error => console.error(error))
+```
+
+Then, you can transport your data:
+
+```javascript
+
+/* 
+Copying and converting from Firebase Authentication data
+ */
+ 
+    bigExport.copyToBigQuery()
+    .then(res => {
+        console.log('Copied ' + res + ' documents to BigQuery.')
+    })
+    .catch(error => console.error(error))
+```
+
+After that, you may want to refresh your data. For the time being, the quick and dirty way is to delete your tables and make new ones:
+
+```javascript
+// Deleting the given BigQuery tables.
+
+bigExport.deleteBigQueryTables()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error => console.error(error))
+```
+
+Maybe you want to refresh / overwrite a single table?
+```javascript
+// Overwrites the users BigQuery table with fresh data from Firebase Authentication
+
+bigExport.deleteBigQueryTables()
+    .then(() => {
+        bigExport.createBigQueryTables()
+    })
+    .then(promises => {
+      return bigExport.copyToBigQuery()
+    })
+    .then(res => {
+      console.log('Copied to BigQuery.')
+    })
+    .catch(error => console.error(error))
+```
+
+## Issues
+Please use the [issue tracker](https://github.com/keito5656/firebase-authentication-to-bigquery-export/issues).
---
+++ index.ts
@@ -0,0 +1,163 @@
+
+import * as admin from 'firebase-admin';
+import * as BigQuery from '@google-cloud/bigquery';
+
+let bigQuery: BigQuery.BigQuery | undefined = undefined
+let app: admin.app.App | undefined = undefined
+const datasetID = "authentication"
+const tableName = "authentication"
+
+/**
+ * Connecting to the given Firebase project.
+ *
+ * @param {JSON} serviceAccountFile
+ * @public
+ */
+
+export function setFirebaseConfig(serviceAccountFile: any) {
+    const keys = {
+        credential: admin.credential.cert(serviceAccountFile),
+    }
+    app = admin.initializeApp(keys, "authentication-to-bigquery-export");
+}
+
+/**
+ * Connecting to the given BigQuery project.
+ *
+ * @param {JSON} serviceAccountFile
+ * @public
+ */
+
+export function setBigQueryConfig(serviceAccountFile: any) {
+    bigQuery = new BigQuery.BigQuery({
+        projectId: serviceAccountFile.project_id,
+        credentials: serviceAccountFile
+    })
+}
+
+
+/**
+ * Creating a BigQuery dataset.
+ * Creating the tables with the correct schema if the table doesn't already exist.
+ *
+ * @public
+ */
+
+export async function createBigQueryTables() {
+    if (!bigQuery) {
+        console.error("please setBigQueryConfig")
+        return
+    }
+    const datasetIsExists = await bigQuery.dataset(datasetID).exists()
+    if (!datasetIsExists[0]) {
+        await bigQuery.createDataset(datasetID)
+    }
+    const options = {
+        name: undefined,
+        friendlyName: "auth",
+        partitioning: undefined,
+        view: undefined,
+        schema: "userId:string, mail:string, creationTime:integer, lastSignInTime:integer, emailVerified:boolean, disabled:boolean, displayName:string, photoURL:string, phoneNumber:string, tokensValidAfterTime:integer"
+    }
+
+    return bigQuery.dataset(datasetID).createTable(tableName, options)
+}
+
+/**
+ * Deletes all the given tables.
+ *
+ * @public
+ */
+export async function deleteBigQueryTables() {
+    if (!bigQuery) {
+        console.error("please setBigQueryConfig")
+        return
+    }
+    const datasetIsExists = await bigQuery.dataset(datasetID).exists()
+
+    if (!datasetIsExists[0]) {
+        console.log("Not found Dataset: " + datasetID)
+        return
+    }
+
+    const tableIsExists = await bigQuery.dataset(datasetID).table(tableName).exists()
+    if (!tableIsExists[0]) {
+        console.log("Not found table: " + tableName)
+        return
+    }
+    return bigQuery.dataset(datasetID).table(tableName).delete()
+}
+
+/**
+ * Runs through the given QuerySnapshot and converts and copies it to an array.
+ * Inserts the array into a BigQuery table with the given collectionName.
+ *
+ * @param {boolean} [verbose = false]
+ * @returns {Promise<Number>}
+ * @public
+ */
+
+export async function copyToBigQuery(verbose = false) {
+    if (!bigQuery || !app) {
+        console.error("please setBigQueryConfig and setFirebaseConfig")
+        return
+    }
+
+    const results = await app.auth().listUsers(1000)
+    let pageToken = results.pageToken
+    let users = results.users
+
+    if (pageToken) {
+        while (pageToken) {
+            const results = await app.auth().listUsers(1000, pageToken)
+            pageToken = results.pageToken
+            users = users.concat(results.users)
+        }
+    }
+    if (verbose) { console.log("Done: fetch user data") }
+
+    const rows = users.map(x => {
+        const creationTime = new Date(x.metadata.creationTime).getTime() / 1000
+        const lastSignInTime = new Date(x.metadata.lastSignInTime).getTime() / 1000
+        let tokensValidAfterTime: number | null = null
+        if (x.tokensValidAfterTime) {
+            tokensValidAfterTime = new Date(x.tokensValidAfterTime).getTime() / 1000
+        }
+
+        return {
+            userId: x.uid,
+            mail: x.email ? x.email : null,
+            emailVerified: x.emailVerified,
+            disabled: x.disabled,
+            displayName: x.displayName ? x.displayName : null,
+            phoneNumber: x.phoneNumber ? x.phoneNumber : null,
+            photoURL: x.photoURL ? x.photoURL : null,
+            tokensValidAfterTime: tokensValidAfterTime,
+            creationTime: creationTime,
+            lastSignInTime: lastSignInTime
+        }
+    })
+
+    if (verbose) { console.log("inserting data ...") }
+
+    return bigQuery.dataset(datasetID).table(tableName).insert(rows)
+        .then(() => {
+            if (verbose) console.log('Successfully copied authentication to BigQuery.')
+        })
+        .catch(e => {
+            let errorMessage = ''
+
+            if (e.errors.length) {
+                errorMessage = e.errors.length + ' errors.'
+                console.error(e.errors.length + ' errors. Here are the first three:')
+                console.error(e.errors[0])
+                console.error(e.errors[1])
+                console.error(e.errors[2])
+            }
+            else {
+                errorMessage = e
+                console.error(e)
+            }
+            throw new Error(errorMessage)
+        })
+}
